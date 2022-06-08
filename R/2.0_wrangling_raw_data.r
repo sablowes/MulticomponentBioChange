c57bfe8f5435697b3571d95439022854e04e66cb
cachaos <cawamaa@gmx.de>
2022-06-09 00:35:17 +0200

    change folder structure

--- R/2.0_wrangling_raw_data.r
+++
@@ -1,5 +0,0 @@
-# Wrangle raw data
-
-if(!dir.exists('data/wrangled data/'))   dir.create('data/wrangled data/')
-listF <- list.files('R/data wrangling', pattern = ".R|.r", full.names = TRUE)
-lapply(listF, function(fullPath) source(fullPath, encoding = 'UTF-8', echo = FALSE, local = TRUE))
